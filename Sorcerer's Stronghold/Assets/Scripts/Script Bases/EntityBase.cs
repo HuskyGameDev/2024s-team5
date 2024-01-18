3b6eae4c0c850cbfebd3a161e42a1b00dbfdc077
Nolan A Casey <ncasey113@gmail.com>
2024-01-17 20:44:04 -0500

    Began work on base scripts
    - Added EntityBase, EnemyBase, and TowerBase
    - Added basic attributes to EntityBase
    - Added elemental descriptions to EntityBase
    - Added damage receiving to EntityBase
    - Added OnDeath events to EntityBase

---
+++ Sorcerer's Stronghold/Assets/Scripts/Script Bases/EntityBase.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Stronghold.Base{
+    public class EntityBase : MonoBehaviour{
+        //These are the elements added in the game
+        public enum Elements{
+            Null,
+            Normal,
+            Fire,
+            Water,
+            Life,
+            Death,
+            Earth
+        }
+
+        /*
+        These are elemental vulnerabilities and resistances. To use them, you take the element
+        of an entity and use it as an index for the lists. The resulting element will be either
+        its resistance or vulnerability. Elements.Null replaces null as nothing, since enums 
+        cannot be null. Elements.Null should never be used as an actual element.  
+        */
+        public Elements[] resist = {Elements.Null, Elements.Null, Elements.Null, Elements.Fire, Elements.Null, Elements.Life, Elements.Fire};
+        public Elements[] vulnerable = {Elements.Null, Elements.Null, Elements.Water, Elements.Life, Elements.Death, Elements.Null};
+
+        /*
+        Most of this is fairly self explanitory. SerializeField marks a private
+        variable to be editable from the inspector in Unity, without exposing
+        the variables to other scripts. 
+        */
+        [Header("Entity Stats")]
+        [SerializeField] private float maximumHealth = 10f;
+        [SerializeField] private float health = 10f;
+        [SerializeField] private float speed = 1f;
+        [SerializeField] private float damage = 2f;
+        [SerializeField] private Elements element = Elements.Normal;
+        [SerializeField] private float resistanceFactor = 0.5f;
+        [SerializeField] private float vulnerableFactor = 1.5f;
+
+        /*
+        This will be a shared method across all entities. Anything that has a health
+        bar can be damaged, or healed, by calling alterHealth. Damage should have
+        negative values, healing should have positive values. 
+        */
+        public void alterHealth(float d, Elements e){
+            if (resist[(int) element] == e){
+                d *= resistanceFactor;
+            }else if (vulnerable[(int) element] == e){
+                d *= vulnerableFactor;
+            }
+            health += d;
+            if (health > maximumHealth){
+                health = maximumHealth;
+            }else if (health <= 0){
+                onDeath();
+            }
+        }
+
+        /*
+        This method alters an entity's health by a percentage of its maximum health.
+        This can be useful if we want to add things of the "heal by 25%" nature. 
+        */
+        public void alterHealthPercentage(float p){
+            health += maximumHealth * p;
+            if (health > maximumHealth){
+                health = maximumHealth;
+            }else if (health <= 0){
+                onDeath();
+            }
+        }
+
+        /*
+        This method allows for different death behaviors. Individual scripts can
+        override this method to have unique behavior. Instead of just dieing, an
+        enemy may explode, or enter a second phase, etc. This method should always
+        call die() at some point.
+        */
+        private void onDeath(){
+            die();
+        }
+
+        /*
+        This method should always just destroy the game object. 
+        */
+        private void die(){
+            Destroy(gameObject);
+        }
+    }
+}
