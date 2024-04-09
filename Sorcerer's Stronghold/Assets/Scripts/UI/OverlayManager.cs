4dd77613dafc16b65a03372d5cbbe0193222318e
Thomas Wilkins <90795014+TW225@users.noreply.github.com>
2024-04-09 11:41:28 -0400

    UI
    Turned the main menu back on and made the healthbar for the crystal update

--- Sorcerer's Stronghold/Assets/Scripts/UI/OverlayManager.cs
+++ Sorcerer's Stronghold/Assets/Scripts/UI/OverlayManager.cs
@@ -1,11 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OverlayManager : MonoBehaviour
 {
+    [Header("Menus")]
     [SerializeField] GameObject statOverlay;
     [SerializeField] GameObject buildOverlay;
+
+    [Header("Player Attributes")]
+    [SerializeField] GameObject player;
+    [SerializeField] Slider playerHealthBar;
+
+    [Header("Tower Attributes")]
+    [SerializeField] GameObject crystal;
+    [SerializeField] Slider crystalHealthBar;
+
+    //grab the components that store health
+    private CenterCrystal cCrys;
+
+    private void Start()
+    {
+        cCrys = crystal.GetComponent<CenterCrystal>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -15,5 +33,17 @@ public class OverlayManager : MonoBehaviour
             buildOverlay.SetActive(false);
         else if(Input.GetKeyDown(KeyCode.B))
             buildOverlay.SetActive(true);
+
+        updateCrystalHealthBar();
+    }
+
+    private void updateCrystalHealthBar()
+    {
+        crystalHealthBar.value = cCrys.getHealth() / cCrys.getMaxHealth();
+    }
+
+    private void updatePlayerHealthBar()
+    {
+
     }
 }
